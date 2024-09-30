4b2d5a38e44113aee2f2b345f88b8ea581d0f248
OJNKHR <wanwan900713@gmail.com>
2024-09-30 18:17:01 +0900

    kadai_004を修正

--- kadai_004/event.js
+++ kadai_004/event.js
@@ -2,7 +2,7 @@ $(function(){
   $(window).on('load',function(){
     console.log('loadイベントが発生しました');   
 });
-  $(document).on('scroll',function(){
+  $(window).on('scroll',function(){
     console.log('scrollイベントが発生しました');   
 });
 });
