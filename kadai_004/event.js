c57385318353da41ee04e3cded6e11deade8fa54
OJNKHR <wanwan900713@gmail.com>
2024-10-01 13:33:56 +0900

    kadai_004を修正

--- kadai_004/event.js
+++ kadai_004/event.js
@@ -1,8 +1,11 @@
+//HTMLが読み込まれたときに実行
+$(window).on('load',function(){
+  console.log('loadイベントが発生しました');   
+});
+
+//ページが読み込まれた後に、scroll後consoleに表示
 $(function(){
-  $(window).on('load',function(){
-    console.log('loadイベントが発生しました');   
-});
   $(window).on('scroll',function(){
-    console.log('scrollイベントが発生しました');   
-});
+      console.log('scrollイベントが発生しました');   
+  }); 
 });
